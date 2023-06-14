7ac829b7293775ee47ddec88132053eb67dda649
Andrew Lee <1517745+andrewrlee@users.noreply.github.com>
2023-06-14 12:05:10 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
+  kotlin("plugin.spring") version "1.8.21"
+}
+
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+}
+
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+}
+
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "19"
+    }
+  }
+}
