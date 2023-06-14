ac4ceaa946e10893c9f8629a814da45c706c5faf
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-06-14 17:53:18 +0100

    Separate integration tests; add jacoco

--- src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/health/HealthCheckTest.kt
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/health/HealthCheckTest.kt
@@ -1,8 +1,8 @@
-package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.integration.health
+package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.health
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.IntegrationTestBase
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.function.Consumer
