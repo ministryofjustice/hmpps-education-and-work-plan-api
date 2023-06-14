660eb80c3cad4421463fb5491eb5859135b0999a
mattwills-moj-digital <mattwills-moj-digital@users.noreply.github.com>
2023-06-14 11:33:58 +0000

    updating project name after deployment from template repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "hmpps-template-kotlin"
+rootProject.name = "hmpps-education-and-work-plan-api"
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppseducationandworkplanapi/HmppsEducationAndWorkPlanApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppseducationandworkplanapi/HmppsEducationAndWorkPlanApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename
+package uk.gov.justice.digital.hmpps.hmppseducationandworkplanapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication()
-class HmppsTemplateKotlin
+class HmppsEducationAndWorkPlanApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<HmppsEducationAndWorkPlanApi>(*args)
 }
