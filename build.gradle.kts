67deecebf1075cbee8f733b572e85929e2fc43da
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2023-06-15 11:21:19 +0100

    Separate integration tests; add jacoco (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0"
   kotlin("plugin.spring") version "1.8.21"
+
+  id("jacoco")
+  id("name.remal.integration-tests") version "4.0.0"
+
+  `java-test-fixtures`
 }
 
 configurations {
@@ -20,5 +25,37 @@ tasks {
     kotlinOptions {
       jvmTarget = "19"
     }
+  }
+}
+
+tasks.named("check") {
+  dependsOn("integrationTest")
+}
+
+// Jacoco code coverage
+tasks.named("test") {
+  finalizedBy("jacocoTestReport")
+}
+tasks.named("integrationTest") {
+  finalizedBy("jacocoIntegrationTestReport")
+}
+
+tasks.named<JacocoReport>("jacocoTestReport") {
+  reports {
+    html.required.set(true)
+  }
+}
+tasks.named<JacocoReport>("jacocoIntegrationTestReport") {
+  reports {
+    html.required.set(true)
+  }
+}
+
+tasks.register<JacocoReport>("combineJacocoReports") {
+  executionData(fileTree(project.buildDir.absolutePath).include("jacoco/*.exec"))
+  classDirectories.setFrom(files(project.sourceSets.main.get().output))
+  sourceDirectories.setFrom(files(project.sourceSets.main.get().allSource))
+  reports {
+    html.required.set(true)
   }
 }
--- src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/IntegrationTestBase.kt
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/IntegrationTestBase.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.integration
+package uk.gov.justice.digital.hmpps.educationandworkplanapi.app
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
