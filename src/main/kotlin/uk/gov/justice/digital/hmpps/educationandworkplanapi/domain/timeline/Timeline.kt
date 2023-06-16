26b8489f8881ec47b60836c50a27629c97fcc033
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-06-16 09:42:43 +0100

    Simplified the events list in Timeline

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/domain/timeline/Timeline.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/domain/timeline/Timeline.kt
@@ -3,32 +3,22 @@ package uk.gov.justice.digital.hmpps.educationandworkplanapi.domain.timeline
 /**
  * A Timeline is sequenced list of [TimelineEvent]s for a given prisoner. The list can be empty, representing an empty
  * timeline for the prisoner.
- *
- * The list of events cannot be mutated directly, and instead should be mutated through methods such as [addEvent]
  */
 class Timeline(
   val prisonNumber: String,
   events: List<TimelineEvent> = emptyList(),
 ) {
 
-  // _events is the real property and is deliberately private to prevent external access and mutation.
-  // events is a pseudo property that returns the current _events field as a List (ie. non-mutable)
-  private val _events: MutableList<TimelineEvent>
-  val events: List<TimelineEvent>
-    get() = _events.toList()
+  val events: MutableList<TimelineEvent>
+    get() = field.also { events -> events.sortByDescending { it.eventDateTime } }
 
   init {
-    _events = events.toMutableList().apply { sortByDescending { it.eventDateTime } }
+    this.events = events.toMutableList()
   }
 
   /**
-   * Adds a [TimelineEvent] to the timeline, ensuring that the events are
-   * in chronological order
+   * Adds a [TimelineEvent] to the timeline.
    */
-  fun addEvent(timelineEvent: TimelineEvent) {
-    with(_events) {
-      add(timelineEvent)
-      sortByDescending { it.eventDateTime }
-    }
-  }
+  fun addEvent(timelineEvent: TimelineEvent) =
+    events.add(timelineEvent)
 }
