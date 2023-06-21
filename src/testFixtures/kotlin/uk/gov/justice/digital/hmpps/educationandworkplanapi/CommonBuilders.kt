90a9bfc858ded8cfbc08e5838c7b084f61ae0f58
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-06-21 08:07:02 +0100

    Added the prisonNumber to the domain Goal service and persistence adapter methods

---
+++ src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/CommonBuilders.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.educationandworkplanapi
+
+/**
+ * Builder functions for common data items that are not aligned to a specific domain, REST model or JPA entity; such as
+ * prison numbers, times and dates etc.
+ */
+
+fun aValidPrisonNumber() = "A1234BC"
