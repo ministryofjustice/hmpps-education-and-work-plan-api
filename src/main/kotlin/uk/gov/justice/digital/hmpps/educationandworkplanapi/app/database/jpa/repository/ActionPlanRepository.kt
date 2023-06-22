96fd9577c7429d365106b6601c6a739f019c4909
Matt Wills <matt.wills@digital.justice.gov.uk>
2023-06-22 11:03:36 +0100

    Fix typo

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/database/jpa/repository/ActionPlanRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/database/jpa/repository/ActionPlanRepository.kt
@@ -3,9 +3,10 @@ package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.database.jpa.re
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.database.jpa.entity.ActionPlanEntity
+import java.util.UUID
 
 @Repository
-interface ActionPlanRepository : JpaRepository<ActionPlanEntity, String> {
+interface ActionPlanRepository : JpaRepository<ActionPlanEntity, UUID> {
 
   fun findByPrisonNumber(prisonNumber: String): ActionPlanEntity?
 }
