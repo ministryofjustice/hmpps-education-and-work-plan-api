a4e2ffd271acdee3e2d2f7584f33157283d05fd1
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-07-25 14:41:20 +0100

    RR-106 - Fixed flyway migration

--- src/main/resources/db/migration/V2023.07.25.0001__add_audit_display_name_fields_to_goal.sql
+++ src/main/resources/db/migration/V2023.07.25.0001__add_audit_display_name_fields_to_goal.sql
@@ -4,5 +4,5 @@ ALTER TABLE goal ADD COLUMN updated_by_display_name VARCHAR(100);
 UPDATE goal SET created_by_display_name = created_by;
 UPDATE goal SET updated_by_display_name = updated_by;
 
-ALTER TABLE goal ALTER COLUMN created_by_display_name VARCHAR(100) NOT NULL;
-ALTER TABLE goal ALTER COLUMN updated_by_display_name VARCHAR(100) NOT NULL;
+ALTER TABLE goal ALTER COLUMN created_by_display_name SET NOT NULL;
+ALTER TABLE goal ALTER COLUMN updated_by_display_name SET NOT NULL;
