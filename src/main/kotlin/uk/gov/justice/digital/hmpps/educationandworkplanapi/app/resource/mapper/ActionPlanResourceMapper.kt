e5bf58546598435556e5e4305488544e3f75a3ba
Matt Wills <matt.wills@digital.justice.gov.uk>
2023-08-16 17:57:08 +0100

    RR-230 - Endpoint to create an Action Plan

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/mapper/ActionPlanResourceMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/mapper/ActionPlanResourceMapper.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.mapper
 
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.domain.goal.ActionPlan
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.ActionPlanResponse
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.CreateActionPlanRequest
 
 @Mapper(
   uses = [
@@ -10,5 +12,9 @@ import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.Actio
   ],
 )
 interface ActionPlanResourceMapper {
+
+  @Mapping(target = "reference", expression = "java(UUID.randomUUID())")
+  fun fromModelToDomain(prisonNumber: String, request: CreateActionPlanRequest): ActionPlan
+
   fun fromDomainToModel(actionPlan: ActionPlan): ActionPlanResponse
 }
