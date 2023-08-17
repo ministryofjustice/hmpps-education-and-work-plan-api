9e94c3b0183e92e5c130033c83f7a8f7ea02ecc3
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-08-17 15:44:12 +0100

    RR-237 - Merge custom dimensions from those already set via the opentelemetry API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/TelemetryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/TelemetryService.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service
 
 import com.microsoft.applicationinsights.TelemetryClient
-import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.baggage.Baggage
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.config.trackEvent
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.domain.goal.Goal
@@ -19,17 +19,30 @@ class TelemetryService(
   }
 
   fun trackGoalCreateEvent(goal: Goal) {
-    telemetryClient.trackEvent(GOAL_CREATE_EVENT, goal.goalCreateEventCustomDimensions())
-    with(Span.current()) {
-      goal.goalCreateEventCustomDimensions().forEach { (key, value) -> setAttribute(key, value) }
-      addEvent("$GOAL_CREATE_EVENT-via-span")
-    }
+    telemetryClient.trackEvent(
+      GOAL_CREATE_EVENT,
+      getExistingCustomDimensions().plus(createEventCustomDimensions(goal)),
+    )
   }
 
-  private fun Goal.goalCreateEventCustomDimensions(): Map<String, String> =
-    mapOf(
-      "status" to this.status.name,
-      "stepCount" to this.steps.size.toString(),
-      "reference" to this.reference.toString(),
-    )
+  /**
+   * Returns a map of data representing the custom dimensions for the `goal-create` event.
+   */
+  private fun createEventCustomDimensions(goal: Goal): Map<String, String> =
+    with(goal) {
+      mapOf(
+        "status" to status.name,
+        "stepCount" to steps.size.toString(),
+        "reference" to reference.toString(),
+      )
+    }
+
+  /**
+   * Returns a map of data representing any custom dimensions already set via the opentelemetry API.
+   */
+  private fun getExistingCustomDimensions(): Map<String, String> =
+    Baggage.current().asMap().mapValues {
+      val baggageEntry = it.value
+      baggageEntry.value
+    }
 }
