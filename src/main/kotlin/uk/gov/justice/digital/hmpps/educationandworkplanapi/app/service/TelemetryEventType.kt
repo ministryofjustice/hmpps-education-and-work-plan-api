860411dbe1fb18fbbd5864f21d598f2b4236ff64
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-09-28 11:38:33 +0100

    RR-307 - TelemetryEventType now responsible for the events custom dimensions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/TelemetryEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/TelemetryEventType.kt
@@ -1,18 +1,58 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service
 
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.domain.goal.Goal
+
 /**
- * An enumeration of the types of Goal events that can be sent to the Telemetry Service.
+ * An enumeration of the types of Goal events that can be sent to the Telemetry Service, where each item has:
+ *   * a text field `value` that is the name of the customEvent recorded in App Insights.
+ *   * a function `customDimensions` that returns the customDimensions that are recorded with the App Insights event.
  */
-enum class TelemetryEventType(val value: String) {
-  GOAL_CREATED("goal-created"),
-  GOAL_UPDATED("goal-updated"),
-  GOAL_STARTED("goal-started"),
-  GOAL_COMPLETED("goal-completed"),
-  GOAL_ARCHIVED("goal-archived"),
-  STEP_UPDATED("step-updated"),
-  STEP_NOT_STARTED("step-not-started"),
-  STEP_STARTED("step-started"),
-  STEP_COMPLETED("step-completed"),
-  STEP_REMOVED("step-removed"),
-  STEP_ADDED("step-added"),
+enum class TelemetryEventType(val value: String, val customDimensions: (goal: Goal) -> Map<String, String>) {
+  GOAL_CREATED(
+    "goal-created",
+    {
+      mapOf(
+        "status" to it.status.name,
+        "stepCount" to it.steps.size.toString(),
+        "reference" to it.reference.toString(),
+        "notesCharacterCount" to (it.notes?.length ?: 0).toString(),
+      )
+    },
+  ),
+
+  GOAL_UPDATED(
+    "goal-updated",
+    {
+      mapOf(
+        "reference" to it.reference.toString(),
+        "notesCharacterCount" to (it.notes?.length ?: 0).toString(),
+      )
+    },
+  ),
+
+  GOAL_STARTED("goal-started", { emptyMap() }),
+
+  GOAL_COMPLETED("goal-completed", { emptyMap() }),
+
+  GOAL_ARCHIVED("goal-archived", { emptyMap() }),
+
+  STEP_UPDATED("step-updated", { emptyMap() }),
+
+  STEP_NOT_STARTED("step-not-started", { emptyMap() }),
+
+  STEP_STARTED("step-started", { emptyMap() }),
+
+  STEP_COMPLETED("step-completed", { emptyMap() }),
+
+  STEP_REMOVED(
+    "step-removed",
+    {
+      mapOf(
+        "reference" to it.reference.toString(),
+        "stepCount" to it.steps.size.toString(),
+      )
+    },
+  ),
+
+  STEP_ADDED("step-added", { emptyMap() }),
 }
