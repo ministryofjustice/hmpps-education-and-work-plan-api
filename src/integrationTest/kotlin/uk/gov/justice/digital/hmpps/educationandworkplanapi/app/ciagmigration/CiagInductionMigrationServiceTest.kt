e0a59f13b2f464cd5e3d93c8813231c0672b69d8
Matt Wills <matt.wills@digital.justice.gov.uk>
2023-11-22 20:04:44 +0000

    RR-503 - Additional test assertions

--- src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/ciagmigration/CiagInductionMigrationServiceTest.kt
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/ciagmigration/CiagInductionMigrationServiceTest.kt
@@ -9,6 +9,15 @@ import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.aValidPrisonNumber
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.anotherValidPrisonNumber
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.HighestEducationLevel.SECONDARY_SCHOOL_TOOK_EXAMS
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.HopingToWork
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.InPrisonTrainingType
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.InPrisonWorkType
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.InterestType
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.SkillType
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.TrainingType
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.WorkExperienceType
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.entity.WorkInterestType
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.jpa.repository.InductionMigrationRepository
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.ciagmigration.resource.model.aValidCiagInductionResponse
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.database.jpa.entity.timeline.TimelineEventEntity
@@ -57,10 +66,28 @@ class CiagInductionMigrationServiceTest : IntegrationTestBase() {
     val importedInduction1 = inductionMigrationRepository.findByPrisonNumber(prisonNumber1)
     assertThat(importedInduction1).isNotNull
     assertThat(importedInduction1!!.prisonNumber).isEqualTo(prisonNumber1)
+    assertThat(importedInduction1.workOnRelease!!.hopingToWork).isEqualTo(HopingToWork.NOT_SURE)
+    assertThat(importedInduction1.previousQualifications!!.educationLevel).isEqualTo(SECONDARY_SCHOOL_TOOK_EXAMS)
+    assertThat(importedInduction1.previousTraining!!.trainingTypes).containsExactly(TrainingType.OTHER)
+    assertThat(importedInduction1.previousWorkExperiences!!.experiences!![0].experienceType).isEqualTo(WorkExperienceType.OTHER)
+    assertThat(importedInduction1.inPrisonInterests!!.inPrisonWorkInterests!![0].workType).isEqualTo(InPrisonWorkType.OTHER)
+    assertThat(importedInduction1.inPrisonInterests!!.inPrisonTrainingInterests!![0].trainingType).isEqualTo(InPrisonTrainingType.OTHER)
+    assertThat(importedInduction1.personalSkillsAndInterests!!.skills!![0].skillType).isEqualTo(SkillType.OTHER)
+    assertThat(importedInduction1.personalSkillsAndInterests!!.interests!![0].interestType).isEqualTo(InterestType.OTHER)
+    assertThat(importedInduction1.futureWorkInterests!!.interests!![0].workType).isEqualTo(WorkInterestType.OTHER)
 
     val importedInduction2 = inductionMigrationRepository.findByPrisonNumber(prisonNumber2)
     assertThat(importedInduction2).isNotNull
     assertThat(importedInduction2!!.prisonNumber).isEqualTo(prisonNumber2)
+    assertThat(importedInduction2.workOnRelease!!.hopingToWork).isEqualTo(HopingToWork.NOT_SURE)
+    assertThat(importedInduction2.previousQualifications!!.educationLevel).isEqualTo(SECONDARY_SCHOOL_TOOK_EXAMS)
+    assertThat(importedInduction2.previousTraining!!.trainingTypes).containsExactly(TrainingType.OTHER)
+    assertThat(importedInduction2.previousWorkExperiences!!.experiences!![0].experienceType).isEqualTo(WorkExperienceType.OTHER)
+    assertThat(importedInduction2.inPrisonInterests!!.inPrisonWorkInterests!![0].workType).isEqualTo(InPrisonWorkType.OTHER)
+    assertThat(importedInduction2.inPrisonInterests!!.inPrisonTrainingInterests!![0].trainingType).isEqualTo(InPrisonTrainingType.OTHER)
+    assertThat(importedInduction2.personalSkillsAndInterests!!.skills!![0].skillType).isEqualTo(SkillType.OTHER)
+    assertThat(importedInduction2.personalSkillsAndInterests!!.interests!![0].interestType).isEqualTo(InterestType.OTHER)
+    assertThat(importedInduction2.futureWorkInterests!!.interests!![0].workType).isEqualTo(WorkInterestType.OTHER)
   }
 
   fun aValidInductionCreatedTimelineEntity(
