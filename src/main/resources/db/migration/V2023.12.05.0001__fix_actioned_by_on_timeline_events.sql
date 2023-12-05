37f087c44f47d9da372c43fd9028119aec75057a
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2023-12-05 16:32:20 +0000

    RR-476 - Update flyway migration to correct any existing timeline event records

--- src/main/resources/db/migration/V2023.12.05.0001__fix_actioned_by_on_timeline_events.sql
+++ src/main/resources/db/migration/V2023.12.05.0001__fix_actioned_by_on_timeline_events.sql
@@ -1,15 +1,15 @@
 -- Update the actioned_by field on timeline events where it has been set incorrectly previously.
 UPDATE timeline_event
-    SET actioned_by = (
-        SELECT g.updated_by
-        FROM timeline_event te
-            INNER JOIN goal g ON g.reference::text = te.source_reference
-        WHERE te.reference = timeline_event.reference
-    )
-    WHERE EXISTS (
-        SELECT *
-        FROM timeline_event te
-            INNER JOIN goal g ON g.reference::text = te.source_reference
-        WHERE (te.event_type = 'GOAL_UPDATED' OR te.event_type = 'INDUCTION_UPDATED')
-          AND te.actioned_by != g.updated_by
-    );
+SET actioned_by = (
+    SELECT g.updated_by
+    FROM timeline_event te
+      INNER JOIN goal g ON g.reference::text = te.source_reference
+    WHERE te.reference = timeline_event.reference
+)
+WHERE id = (
+    SELECT te.id
+    FROM timeline_event te
+      INNER JOIN goal g ON g.reference::text = te.source_reference
+    WHERE (te.event_type = 'GOAL_UPDATED' OR te.event_type = 'INDUCTION_UPDATED')
+      AND te.actioned_by != g.updated_by
+);
