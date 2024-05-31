88b9ffe25868f756603c5298345cc876ae96f26f
Lee Jacobson <lee.jacobson@digital.justice.gov.uk>
2024-05-31 13:44:45 +0100

    RR-838: Updated uk.gov.justice.hmpps.gradle-spring-boot to v6 and hmpps-kotlin-spring-boot-starter library to v1

--- domain/learningandworkprogress/build.gradle.kts
+++ domain/learningandworkprogress/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
   `java-library`
   `java-test-fixtures`
@@ -35,10 +38,8 @@ java {
 }
 
 tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-      jvmTarget = "21"
-    }
+  withType<KotlinCompile> {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
   }
 }
 
