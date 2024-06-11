9a5eea93cba61dc92c547bbb77f518d391ca76b3
james-baker-digital-justice <159048573+james-baker-digital-justice@users.noreply.github.com>
2024-06-11 17:09:18 +0100

    RR-845: Add "not relevant" option to previous work experience (#298)

--- src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/resource/model/induction/UpdatePreviousWorkExperiencesRequestBuilder.kt
+++ src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/resource/model/induction/UpdatePreviousWorkExperiencesRequestBuilder.kt
@@ -1,12 +1,13 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.induction
 
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.HasWorkedBefore
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.PreviousWorkExperience
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.UpdatePreviousWorkExperiencesRequest
 import java.util.UUID
 
 fun aValidUpdatePreviousWorkExperiencesRequest(
   reference: UUID? = UUID.randomUUID(),
-  hasWorkedBefore: Boolean = true,
+  hasWorkedBefore: HasWorkedBefore = HasWorkedBefore.YES,
   experiences: List<PreviousWorkExperience>? = listOf(aValidPreviousWorkExperience()),
 ): UpdatePreviousWorkExperiencesRequest =
   UpdatePreviousWorkExperiencesRequest(
