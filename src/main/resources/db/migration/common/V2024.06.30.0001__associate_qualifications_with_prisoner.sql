ccc06bd3de22749e1eae24fbfbe5a0dd089d09c6
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-07-31 14:24:56 +0100

    RR-900 - Fixed flyway migration

--- src/main/resources/db/migration/common/V2024.06.30.0001__associate_qualifications_with_prisoner.sql
+++ src/main/resources/db/migration/common/V2024.06.30.0001__associate_qualifications_with_prisoner.sql
@@ -1,11 +1,12 @@
 --- Associate previous qualifications with the prisoner (disassociating it from the induction) by:
----   1. Add `prison_number` column to `previous_qualifications`
+---   1. Add `prison_number` column to `previous_qualifications` allowing nulls
 ---   2. Populate `previous_qualifications.prison_number` from the associated induction record
----   3. Drop the column `induction.previous_qualifications_id`
+---   3. Make the new `prison_number` column non-nullable
+---   4. Drop the column `induction.previous_qualifications_id`
 
 
 --- 1. Add `prison_number` column to `previous_qualifications`
-ALTER TABLE previous_qualifications ADD COLUMN prison_number VARCHAR(10) NOT NULL;
+ALTER TABLE previous_qualifications ADD COLUMN prison_number VARCHAR(10);
 CREATE UNIQUE INDEX previous_qualifications_prison_number_idx ON previous_qualifications
 (
     prison_number
@@ -19,6 +20,9 @@ SET prison_number = (
     WHERE i.previous_qualifications_id = pq.id
 );
 
---- 3. Drop the column `induction.previous_qualifications_id`
+--- 3. Add non-null constraint to `previous_qualifications.prison_number`
+ALTER TABLE previous_qualifications ALTER COLUMN prison_number SET NOT NULL;
+
+--- 4. Drop the column `induction.previous_qualifications_id`
 ALTER TABLE induction
 DROP COLUMN previous_qualifications_id;
