8d273ac401c76e550756e521601f67f80c8e8690
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-08-13 11:45:49 +0100

    RR-901 - Update swagger spec and introduce test fixtures

---
+++ src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/resource/model/education/AchievedQualificationResponseBuilder.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.education
+
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.AchievedQualificationResponse
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.QualificationLevel
+import java.time.OffsetDateTime
+import java.util.UUID
+
+fun aValidAchievedQualificationResponse(
+  reference: UUID = UUID.randomUUID(),
+  subject: String = "English",
+  level: QualificationLevel = QualificationLevel.LEVEL_3,
+  grade: String = "A",
+  createdBy: String = "asmith_gen",
+  createdAt: OffsetDateTime = OffsetDateTime.now(),
+  updatedBy: String = "asmith_gen",
+  updatedAt: OffsetDateTime = OffsetDateTime.now(),
+): AchievedQualificationResponse =
+  AchievedQualificationResponse(
+    reference = reference,
+    subject = subject,
+    level = level,
+    grade = grade,
+    createdBy = createdBy,
+    createdAt = createdAt,
+    updatedBy = updatedBy,
+    updatedAt = updatedAt,
+  )
+
+fun anotherValidAchievedQualificationResponse(
+  reference: UUID = UUID.randomUUID(),
+  subject: String = "Maths",
+  level: QualificationLevel = QualificationLevel.LEVEL_3,
+  grade: String = "B",
+  createdBy: String = "asmith_gen",
+  createdAt: OffsetDateTime = OffsetDateTime.now(),
+  updatedBy: String = "asmith_gen",
+  updatedAt: OffsetDateTime = OffsetDateTime.now(),
+): AchievedQualificationResponse =
+  aValidAchievedQualificationResponse(
+    reference = reference,
+    subject = subject,
+    level = level,
+    grade = grade,
+    createdBy = createdBy,
+    createdAt = createdAt,
+    updatedBy = updatedBy,
+    updatedAt = updatedAt,
+  )
---
+++ src/testFixtures/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/resource/model/education/EducationResponseAssert.kt
@@ -0,0 +1,178 @@
+package uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.education
+
+import org.assertj.core.api.AbstractObjectAssert
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.AchievedQualificationResponse
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.EducationLevel
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.EducationResponse
+import java.time.OffsetDateTime
+import java.util.UUID
+import java.util.function.Consumer
+
+fun assertThat(actual: EducationResponse?) = EducationResponseAssert(actual)
+
+/**
+ * AssertJ custom assertion for a single [EducationResponse].
+ */
+class EducationResponseAssert(actual: EducationResponse?) :
+  AbstractObjectAssert<EducationResponseAssert, EducationResponse?>(
+    actual,
+    EducationResponseAssert::class.java,
+  ) {
+
+  fun hasReference(expected: UUID): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (reference != expected) {
+        failWithMessage("Expected reference to be $expected, but was $reference")
+      }
+    }
+    return this
+  }
+
+  fun hasEducationLevel(expected: EducationLevel): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (educationLevel != expected) {
+        failWithMessage("Expected educationLevel to be $expected, but was $educationLevel")
+      }
+    }
+    return this
+  }
+
+  fun hasQualifications(expected: List<AchievedQualificationResponse>): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (qualifications != expected) {
+        failWithMessage("Expected qualifications to be $expected, but was $qualifications")
+      }
+    }
+    return this
+  }
+
+  fun hasNumberOfQualifications(expected: Int): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (qualifications.size != expected) {
+        failWithMessage("Expected number of qualifications to be $expected, but was ${qualifications.size}")
+      }
+    }
+    return this
+  }
+
+  /**
+   * Allows for assertion chaining into the specified child [AchievedQualificationResponse]. Takes a lambda as the method argument
+   * to call assertion methods provided by [AchievedQualificationResponseAssert].
+   * Returns this [EducationResponseAssert] to allow further chained assertions on the parent [EducationResponse]
+   *
+   * The `qualificationNumber` parameter is not zero indexed to make for better readability in tests. IE. the first qualification
+   * should be referenced as `.qualification(1) { .... }`
+   */
+  fun qualification(qualificationNumber: Int, consumer: Consumer<AchievedQualificationResponseAssert>): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      val qualification = this.qualifications[qualificationNumber - 1]
+      consumer.accept(assertThat(qualification))
+    }
+    return this
+  }
+
+  fun wasCreatedAt(expected: OffsetDateTime): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (createdAt != expected) {
+        failWithMessage("Expected createdAt to be $expected, but was $createdAt")
+      }
+    }
+    return this
+  }
+
+  fun wasCreatedAfter(dateTime: OffsetDateTime): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (!createdAt.isAfter(dateTime)) {
+        failWithMessage("Expected createdAt to be after $dateTime, but was $createdAt")
+      }
+    }
+    return this
+  }
+
+  fun wasUpdatedAt(expected: OffsetDateTime): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (updatedAt != expected) {
+        failWithMessage("Expected updatedAt to be $expected, but was $updatedAt")
+      }
+    }
+    return this
+  }
+
+  fun wasUpdatedAfter(dateTime: OffsetDateTime): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (!updatedAt.isAfter(dateTime)) {
+        failWithMessage("Expected updatedAt to be after $dateTime, but was $updatedAt")
+      }
+    }
+    return this
+  }
+
+  fun wasCreatedBy(expected: String): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (createdBy != expected) {
+        failWithMessage("Expected createdBy to be $expected, but was $createdBy")
+      }
+    }
+    return this
+  }
+
+  fun wasCreatedByDisplayName(expected: String): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (createdByDisplayName != expected) {
+        failWithMessage("Expected createdByDisplayName to be $expected, but was $createdByDisplayName")
+      }
+    }
+    return this
+  }
+
+  fun wasUpdatedBy(expected: String): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (updatedBy != expected) {
+        failWithMessage("Expected updatedBy to be $expected, but was $updatedBy")
+      }
+    }
+    return this
+  }
+
+  fun wasUpdatedByDisplayName(expected: String): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (updatedByDisplayName != expected) {
+        failWithMessage("Expected updatedByDisplayName to be $expected, but was $updatedByDisplayName")
+      }
+    }
+    return this
+  }
+
+  fun wasCreatedAtPrison(expected: String): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (createdAtPrison != expected) {
+        failWithMessage("Expected createdAtPrison to be $expected, but was $createdAtPrison")
+      }
+    }
+    return this
+  }
+
+  fun wasUpdatedAtPrison(expected: String): EducationResponseAssert {
+    isNotNull
+    with(actual!!) {
+      if (updatedAtPrison != expected) {
+        failWithMessage("Expected updatedAtPrison to be $expected, but was $updatedAtPrison")
+      }
+    }
+    return this
+  }
+}
