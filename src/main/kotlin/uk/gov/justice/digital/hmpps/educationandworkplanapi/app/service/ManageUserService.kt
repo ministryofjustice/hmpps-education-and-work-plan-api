857225e17698c85c44faa3d817fd512f8ef36dda
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2024-10-14 19:46:59 +0100

    RR-1004 - Basic caching of user details lookups

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/ManageUserService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/ManageUserService.kt
@@ -1,13 +1,16 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service
 
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.client.manageusers.ManageUsersApiClient
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.client.manageusers.UserDetailsDto
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.config.CacheConfiguration.Companion.USER_DETAILS
 
 @Component
 class ManageUserService(
   private val manageUsersApiClient: ManageUsersApiClient,
 ) {
+  @Cacheable(USER_DETAILS)
   fun getUserDetails(username: String): UserDetailsDto =
     manageUsersApiClient.getUserDetails(username)
 }
