c6da8dc985389c248381ce9be554e94ba4ed4f90
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2024-10-14 17:15:12 +0100

    RR-1004 - Mapped displayName fields in TimelineEventResourceMapper via user lookup (#391)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/mapper/timeline/TimelineEventResourceMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/mapper/timeline/TimelineEventResourceMapper.kt
@@ -1,26 +1,57 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.mapper.timeline
 
-import org.mapstruct.Mapper
-import org.mapstruct.Mapping
+import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.domain.timeline.TimelineEvent
+import uk.gov.justice.digital.hmpps.domain.timeline.TimelineEventContext
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.mapper.InstantMapper
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service.ManageUserService
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.TimelineEventResponse
-import java.time.Instant
-import java.util.UUID
+import uk.gov.justice.digital.hmpps.domain.timeline.TimelineEventType as TimelineEventTypeDomain
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.TimelineEventType as TimelineEventTypeApi
 
-@Mapper(
-  uses = [
-    InstantMapper::class,
-  ],
-  imports = [
-    Instant::class,
-    UUID::class,
-  ],
-)
-abstract class TimelineEventResourceMapper {
-  @Mapping(target = "contextualInfo", expression = "java( buildContextualInfo(timelineEventDomain) )")
-  abstract fun fromDomainToModel(timelineEventDomain: TimelineEvent): TimelineEventResponse
+@Component
+class TimelineEventResourceMapper(
+  private val instantMapper: InstantMapper,
+  private val userService: ManageUserService,
+) {
+  fun fromDomainToModel(timelineEventDomain: TimelineEvent): TimelineEventResponse =
+    with(timelineEventDomain) {
+      TimelineEventResponse(
+        reference = reference,
+        sourceReference = sourceReference,
+        correlationId = correlationId,
+        eventType = toTimelineEventType(eventType),
+        prisonId = prisonId,
+        contextualInfo = buildContextualInfo(contextualInfo),
+        actionedBy = actionedBy,
+        timestamp = instantMapper.toOffsetDateTime(timestamp)!!,
+        actionedByDisplayName = userService.getUserDetails(actionedBy).let {
+          if (it.active) it.name else null
+        },
+      )
+    }
 
-  protected fun buildContextualInfo(timelineEventDomain: TimelineEvent): Map<String, String> =
-    timelineEventDomain.contextualInfo?.mapKeys { it.key.toString() } ?: emptyMap()
+  private fun buildContextualInfo(contextualInfo: Map<TimelineEventContext, String>?): Map<String, String> =
+    contextualInfo?.mapKeys { it.key.toString() } ?: emptyMap()
+
+  private fun toTimelineEventType(timelineEventType: TimelineEventTypeDomain): TimelineEventTypeApi =
+    when (timelineEventType) {
+      TimelineEventTypeDomain.INDUCTION_CREATED -> TimelineEventTypeApi.INDUCTION_CREATED
+      TimelineEventTypeDomain.INDUCTION_UPDATED -> TimelineEventTypeApi.INDUCTION_UPDATED
+      TimelineEventTypeDomain.ACTION_PLAN_CREATED -> TimelineEventTypeApi.ACTION_PLAN_CREATED
+      TimelineEventTypeDomain.GOAL_CREATED -> TimelineEventTypeApi.GOAL_CREATED
+      TimelineEventTypeDomain.GOAL_UPDATED -> TimelineEventTypeApi.GOAL_UPDATED
+      TimelineEventTypeDomain.GOAL_COMPLETED -> TimelineEventTypeApi.GOAL_COMPLETED
+      TimelineEventTypeDomain.GOAL_ARCHIVED -> TimelineEventTypeApi.GOAL_ARCHIVED
+      TimelineEventTypeDomain.GOAL_UNARCHIVED -> TimelineEventTypeApi.GOAL_UNARCHIVED
+      TimelineEventTypeDomain.STEP_UPDATED -> TimelineEventTypeApi.STEP_UPDATED
+      TimelineEventTypeDomain.STEP_NOT_STARTED -> TimelineEventTypeApi.STEP_NOT_STARTED
+      TimelineEventTypeDomain.STEP_STARTED -> TimelineEventTypeApi.STEP_STARTED
+      TimelineEventTypeDomain.STEP_COMPLETED -> TimelineEventTypeApi.STEP_COMPLETED
+      TimelineEventTypeDomain.CONVERSATION_CREATED -> TimelineEventTypeApi.CONVERSATION_CREATED
+      TimelineEventTypeDomain.CONVERSATION_UPDATED -> TimelineEventTypeApi.CONVERSATION_UPDATED
+      TimelineEventTypeDomain.PRISON_ADMISSION -> TimelineEventTypeApi.PRISON_ADMISSION
+      TimelineEventTypeDomain.PRISON_RELEASE -> TimelineEventTypeApi.PRISON_RELEASE
+      TimelineEventTypeDomain.PRISON_TRANSFER -> TimelineEventTypeApi.PRISON_TRANSFER
+    }
 }
