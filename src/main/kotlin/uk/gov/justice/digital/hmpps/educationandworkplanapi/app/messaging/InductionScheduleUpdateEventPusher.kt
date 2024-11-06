227ccd69b95fb86c714e57209ec87cc60f00c6af
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2024-11-06 12:14:58 +0000

    Create outbound message on the creation/update of an induction schedule

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/messaging/InductionScheduleUpdateEventPusher.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/messaging/InductionScheduleUpdateEventPusher.kt
@@ -20,6 +20,9 @@ class InductionScheduleUpdateEventPusher(
 ) {
   companion object {
     val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    const val EVENT_TYPE = "plp.induction-schedule.updated"
+    const val CONTENT_TYPE = "text/plain;charset=UTF-8"
   }
 
   internal val eventTopic by lazy { hmppsQueueService.findByTopicId("domainevents") as HmppsTopic }
@@ -27,25 +30,21 @@ class InductionScheduleUpdateEventPusher(
   fun sendEvent(prisonerNumber: String, occurredAt: Instant) {
     val cne = HmppsDomainEvent(prisonerNumber, serviceBaseUrl, occurredAt)
     log.info("Pushing induction schedule updated event to event topic person reference $prisonerNumber")
-    try {
-      val publishResponse = eventTopic.publish(
-        cne.eventType,
-        objectMapper.writeValueAsString(cne),
-        attributes = mapOf(
-          "eventType" to MessageAttributeValue.builder().dataType("String").stringValue(cne.eventType).build(),
-          "contentType" to MessageAttributeValue.builder().dataType("String")
-            .stringValue("text/plain;charset=UTF-8").build(),
-        ),
-      )
-      log.debug("Sent case induction schedule updated with message id {}", publishResponse.messageId())
-    } catch (throwable: Throwable) {
-      log.error("Failed to send induction schedule updated message", throwable)
-    }
+    val publishResponse = eventTopic.publish(
+      cne.eventType,
+      objectMapper.writeValueAsString(cne),
+      attributes = mapOf(
+        "eventType" to MessageAttributeValue.builder().dataType("String").stringValue(cne.eventType).build(),
+        "contentType" to MessageAttributeValue.builder().dataType("String")
+          .stringValue(CONTENT_TYPE).build(),
+      ),
+    )
+    log.debug("Sent case induction schedule updated with message id {}", publishResponse.messageId())
   }
 
   data class HmppsDomainEvent(
     val version: Int = 1,
-    val eventType: String = "plp.induction-schedule.updated",
+    val eventType: String = EVENT_TYPE,
     val description: String = "A prisoner learning plan induction schedule created or amended",
     val detailUrl: String,
     val occurredAt: Instant,
