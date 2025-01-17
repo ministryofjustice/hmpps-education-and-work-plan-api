d9bf1254c476185c6ec2b2101f985a92a794a7ee
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2025-01-17 15:29:54 +0000

    create induction timeline entry when an action plan is created

--- src/test/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/AsyncActionPlanEventServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/AsyncActionPlanEventServiceTest.kt
@@ -11,6 +11,7 @@ import org.mockito.kotlin.given
 import org.mockito.kotlin.verify
 import uk.gov.justice.digital.hmpps.domain.aValidPrisonNumber
 import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.aFullyPopulatedInduction
+import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.service.InductionService
 import uk.gov.justice.digital.hmpps.domain.personallearningplan.aValidActionPlan
 import uk.gov.justice.digital.hmpps.domain.timeline.TimelineEventType
 import uk.gov.justice.digital.hmpps.domain.timeline.aValidTimelineEvent
@@ -31,17 +32,21 @@ class AsyncActionPlanEventServiceTest {
   @Mock
   private lateinit var timelineService: TimelineService
 
+  @Mock
+  private lateinit var inductionService: InductionService
+
   @Test
   fun `should handle action plan created`() {
     // Given
     val prisonNumber = aValidPrisonNumber()
-    val actionPlan = aValidActionPlan()
-    val induction = aFullyPopulatedInduction()
+    val actionPlan = aValidActionPlan(prisonNumber = prisonNumber)
+    val induction = aFullyPopulatedInduction(prisonNumber = prisonNumber)
     val createActionPlanEvents = listOf(
       aValidTimelineEvent(eventType = TimelineEventType.ACTION_PLAN_CREATED),
       aValidTimelineEvent(eventType = TimelineEventType.GOAL_CREATED),
     )
-    given(timelineEventFactory.actionPlanCreatedEvent(any(), induction)).willReturn(createActionPlanEvents)
+    given(timelineEventFactory.actionPlanCreatedEvent(actionPlan, induction)).willReturn(createActionPlanEvents)
+    given(inductionService.getInductionForPrisoner(prisonNumber)).willReturn(induction)
 
     // When
     actionPlanEventService.actionPlanCreated(actionPlan)
