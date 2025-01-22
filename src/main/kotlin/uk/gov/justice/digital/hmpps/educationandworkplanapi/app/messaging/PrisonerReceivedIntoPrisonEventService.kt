1aad73d4f97962b72afdb465a609312086420ddb
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2025-01-22 16:53:41 +0000

    handle prisoner transfer messages wrt inductions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/messaging/PrisonerReceivedIntoPrisonEventService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/messaging/PrisonerReceivedIntoPrisonEventService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.messaging
 import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.InductionScheduleAlreadyExistsException
+import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.InductionScheduleNotFoundException
 import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.InductionScheduleStatus.COMPLETED
 import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.service.InductionScheduleService
 import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.review.ReviewScheduleNotFoundException
@@ -20,6 +21,9 @@ import java.time.LocalDate
 import java.time.ZoneOffset
 
 private val log = KotlinLogging.logger {}
+
+private const val REVIEW_SCHEDULE = "Review Schedule"
+private const val INDUCTION_SCHEDULE = "Induction Schedule"
 
 @Service
 class PrisonerReceivedIntoPrisonEventService(
@@ -70,18 +74,27 @@ class PrisonerReceivedIntoPrisonEventService(
   }
 
   private fun PrisonerReceivedAdditionalInformation.processPrisonerTransferEvent() {
-    log.info { "Processing Prisoner Admission Event (due to transfer) for prisoner [$nomsNumber]" }
+    log.info { "Processing Prisoner Transfer Event for prisoner [$nomsNumber] at prison [$prisonId]" }
 
-    try {
-      reviewScheduleService.exemptAndReScheduleActiveReviewScheduleDueToPrisonerTransfer(
-        prisonNumber = nomsNumber,
-        prisonTransferredTo = prisonId,
-      )
-    } catch (e: ReviewScheduleNotFoundException) {
-      log.debug { "Prisoner [$nomsNumber] does not have an active Review Schedule; no need to exempt and re-schedule it" }
-    }
+    handle(
+      scheduleType = REVIEW_SCHEDULE,
+      action = {
+        reviewScheduleService.exemptAndReScheduleActiveReviewScheduleDueToPrisonerTransfer(
+          prisonNumber = nomsNumber,
+          prisonTransferredTo = prisonId,
+        )
+      },
+    )
 
-    // TODO - RR-1215 - call inductionScheduleService to exempt & reschedule Induction Schedule due to prisoner transfer
+    handle(
+      scheduleType = INDUCTION_SCHEDULE,
+      action = {
+        inductionScheduleService.exemptAndReScheduleActiveInductionScheduleDueToPrisonerTransfer(
+          prisonNumber = nomsNumber,
+          prisonTransferredTo = prisonId,
+        )
+      },
+    )
   }
 
   private fun rescheduleOrCreatePrisonersReviewSchedule(prisoner: Prisoner, prisonId: String) {
@@ -89,7 +102,10 @@ class PrisonerReceivedIntoPrisonEventService(
       runCatching { reviewScheduleService.getActiveReviewScheduleForPrisoner(prisoner.prisonerNumber) }.getOrNull()
     if (reviewSchedule != null) {
       // An active Review Schedule exists - Exempt it for unknown reason, which will make it non-active
-      reviewScheduleService.exemptActiveReviewScheduleStatusDueToUnknownReason(prisonNumber = prisoner.prisonerNumber, prisonId = prisonId)
+      reviewScheduleService.exemptActiveReviewScheduleStatusDueToUnknownReason(
+        prisonNumber = prisoner.prisonerNumber,
+        prisonId = prisonId,
+      )
     }
 
     // Create a new Review Schedule for the prisoner
@@ -100,5 +116,20 @@ class PrisonerReceivedIntoPrisonEventService(
       isTransfer = false,
     )
     reviewScheduleService.createInitialReviewSchedule(reviewScheduleDto)
+  }
+
+  private fun handle(
+    scheduleType: String,
+    action: () -> Unit,
+  ) {
+    try {
+      action()
+    } catch (e: Exception) {
+      if (e is ReviewScheduleNotFoundException || e is InductionScheduleNotFoundException) {
+        log.debug { "Prisoner does not have an active $scheduleType; no need to set it as exempt" }
+      } else {
+        throw e // Re-throw unexpected exceptions
+      }
+    }
   }
 }
