f3c69aa04392ef4a3713e8a573709512ae6fb8cb
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2025-02-03 10:42:46 +0000

    Complete induction schedule

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/ScheduleAdapter.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service
+
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.InductionScheduleStatus
+import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.service.InductionPersistenceAdapter
+import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.induction.service.InductionScheduleService
+import uk.gov.justice.digital.hmpps.domain.learningandworkprogress.review.service.ReviewScheduleService
+import uk.gov.justice.digital.hmpps.domain.personallearningplan.service.ActionPlanPersistenceAdapter
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.mapper.review.CreateInitialReviewScheduleMapper
+
+private val log = KotlinLogging.logger {}
+
+@Service
+class ScheduleAdapter(
+  private val inductionPersistenceAdapter: InductionPersistenceAdapter,
+  private val actionPlanPersistenceAdapter: ActionPlanPersistenceAdapter,
+  private val prisonerSearchApiService: PrisonerSearchApiService,
+  private val createInitialReviewScheduleMapper: CreateInitialReviewScheduleMapper,
+  private val reviewScheduleService: ReviewScheduleService,
+  private val inductionScheduleService: InductionScheduleService,
+) {
+
+  fun completeInductionScheduleAndCreateInitialReviewSchedule(prisonNumber: String) {
+    // validate
+    if (
+      inductionPersistenceAdapter.getInduction(prisonNumber) != null &&
+      actionPlanPersistenceAdapter.getActionPlan(prisonNumber) != null
+    ) {
+      // COMPLETE the induction schedule IF it is not already completed.
+      val inductionSchedule = runCatching {
+        inductionScheduleService.getInductionScheduleForPrisoner(prisonNumber)
+      }.getOrNull()
+      if (inductionSchedule != null && inductionSchedule.scheduleStatus == InductionScheduleStatus.SCHEDULED) {
+        inductionScheduleService.updateInductionSchedule(
+          inductionSchedule = inductionSchedule,
+          newStatus = InductionScheduleStatus.COMPLETED,
+          prisonId = inductionSchedule.lastUpdatedAtPrison,
+        )
+      }
+
+      // Create initial review schedule
+      val prisoner = prisonerSearchApiService.getPrisoner(prisonNumber)
+      val reviewScheduleDto = createInitialReviewScheduleMapper.fromPrisonerToDomain(
+        prisoner = prisoner,
+        isReadmission = false,
+        isTransfer = false,
+      )
+      reviewScheduleService.createInitialReviewSchedule(reviewScheduleDto)
+    }
+  }
+}
