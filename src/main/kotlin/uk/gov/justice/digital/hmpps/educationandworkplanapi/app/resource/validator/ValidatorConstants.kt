ea37fa4cff4b7c9e700788e8d84663dad547ba03
Nathan Russell <nathan.russell1@digital.justice.gov.uk>
2025-02-11 20:28:08 +0000

    RR-1246 - Skeleton controller method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/validator/ValidatorConstants.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/validator/ValidatorConstants.kt
@@ -1,3 +1,5 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.validator
 
 const val PRISON_NUMBER_FORMAT = "^[A-Z]\\d{4}[A-Z]{2}$"
+
+const val PRISON_ID_FORMAT = "^[A-Z]{3}$"
