ab87d9126dcc6e86ff4915b0d1f0c36588bb7903
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-03-27 15:15:26 +0000

    Added retry mechanism when manage users api is not available (#650)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/client/manageusers/ManageUsersApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/client/manageusers/ManageUsersApiClient.kt
@@ -1,9 +1,15 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.client.manageusers
 
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientRequestException
 import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.util.retry.Retry
+import java.time.Duration
+
+private val log = KotlinLogging.logger {}
 
 @Component
 class ManageUsersApiClient(@Qualifier("manageUsersApiWebClient") private val manageUsersApiClient: WebClient) {
@@ -13,6 +19,18 @@ class ManageUsersApiClient(@Qualifier("manageUsersApiWebClient") private val man
       .uri("/users/{username}", username)
       .retrieve()
       .bodyToMono(UserDetailsDto::class.java)
+      .retryWhen(
+        Retry.backoff(3, Duration.ofMillis(500))
+          .filter { ex -> ex is WebClientRequestException }
+          .doBeforeRetry { retrySignal ->
+            log.warn(
+              "Retrying request for user {} due to {} (attempt #{})",
+              username,
+              retrySignal.failure().javaClass.simpleName,
+              retrySignal.totalRetries() + 1,
+            )
+          },
+      )
       .block()!!
   } catch (e: WebClientResponseException.NotFound) {
     UserDetailsDto(username, false, "$username not found")
