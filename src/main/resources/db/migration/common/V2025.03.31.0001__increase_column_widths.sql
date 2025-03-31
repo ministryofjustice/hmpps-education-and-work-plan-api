775ad7677b5a03c8dd0a1d08c2374db794135601
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-03-31 14:21:50 +0100

    Increase size of timeline contextural info (#651)

---
+++ src/main/resources/db/migration/common/V2025.03.31.0001__increase_column_widths.sql
@@ -0,0 +1,15 @@
+--- RR-1318 increase column widths for goal & step titles, and induction & review exemption reasons.
+--- This is because the UI validates at (for example) 512 characters, but a line break in the UI (single character)
+--- accounts for 2 characters in the database (\r\n). Therefore the database columns need to be wider than the UI
+--- validation to account for any line breaks the user has entered in their content.
+--- Setting the column type to TEXT allows for the UI to change/increase its field length requirements in the future.
+---
+--- Strictly speaking this would apply to any text field submitted by the UI, but most are single line UI fields so
+--- line breaks cannot be entered. The only multi-line text fields in the UI are goal and step titles, notes, and
+--- induction & review exemption reasons, though the various 'notes' columns are already of type TEXT so do not need
+--- changing.
+
+--- Additional field missed in the previous commit
+
+ALTER TABLE TIMELINE_EVENT_CONTEXTUAL_INFO
+    ALTER COLUMN VALUE TYPE TEXT;
