f4f6ca464090b053841da9b0a7bcb1d6ceeab7e7
rickchoijd <rick.choi@digital.justice.gov.uk>
2025-06-25 16:23:19 +0100

    ESWE-1338 Revise PES config
    Pull config value `pes.contract-start-date` to config `PrisonEducationServiceProperties` for later reuse

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/AssessmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/AssessmentService.kt
@@ -1,27 +1,25 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service
 
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.client.prisonersearch.MissingReceptionDateException
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.client.prisonersearch.MissingSentenceStartDateException
-import java.time.LocalDate
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.config.PrisonEducationServiceProperties
 
 private val log = KotlinLogging.logger {}
 
 @Service
 class AssessmentService(
   private val prisonerSearchApiService: PrisonerSearchApiService,
-  @Value("\${pes.contract-start-date}") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) private val pesContractStartDate: LocalDate,
+  private val pes: PrisonEducationServiceProperties,
 ) {
   fun requireBasicSkillsAssessment(prisonNumber: String): Boolean {
     log.info { "Checking eligibility of basic skills assessment for prisoner [$prisonNumber]" }
 
     return prisonerSearchApiService.getPrisoner(prisonNumber).let {
       when {
-        (it.receptionDate ?: throw MissingReceptionDateException(prisonNumber)).isBefore(pesContractStartDate) -> false
-        (it.sentenceStartDate ?: throw MissingSentenceStartDateException(prisonNumber)).isBefore(pesContractStartDate) -> false
+        (it.receptionDate ?: throw MissingReceptionDateException(prisonNumber)).isBefore(pes.contractStartDate) -> false
+        (it.sentenceStartDate ?: throw MissingSentenceStartDateException(prisonNumber)).isBefore(pes.contractStartDate) -> false
 
         else -> true
       }
