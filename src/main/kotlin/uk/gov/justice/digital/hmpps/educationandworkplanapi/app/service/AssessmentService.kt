a9335c5f91d5998aba78fa5dfe731dab65e4dd9a
rickchoijd <rick.choi@justice.gov.uk>
2025-09-12 15:25:10 +0100

    ESWE-1375 Add trace logging for BSA assessment eligibility on `dev`

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/AssessmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/AssessmentService.kt
@@ -20,11 +20,20 @@ class AssessmentService(
         ?.filter { offence -> offence.sentenceStartDate != null }
         ?.maxByOrNull { offence -> offence.sentenceStartDate!! }
         ?.sentenceStartDate ?: it.receptionDate
+      log.trace {
+        val latestSentenceStartDate = it.allConvictedOffences
+          ?.filter { offence -> offence.sentenceStartDate != null }
+          ?.maxByOrNull { offence -> offence.sentenceStartDate!! }
+          ?.sentenceStartDate
+        """
+          Input for eligibility check of basic skills assessment for prisoner [$prisonNumber]: referenceDate=$referenceDate, latest sentenceStartDate=$latestSentenceStartDate, receptionDate=${it.receptionDate}
+        """.trimIndent()
+      }
       when {
         referenceDate == null -> throw MissingSentenceStartDateAndReceptionDateException(prisonNumber)
         referenceDate.isBefore(pes.contractStartDate) -> false
         else -> true
-      }
+      }.also { log.trace { "Eligibility of basic skills assessment for prisoner [$prisonNumber]: ${if (it) "IS" else "NOT"} eligible" } }
     }
   }
 }
