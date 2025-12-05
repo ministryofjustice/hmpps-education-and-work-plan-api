76d7b4dfb36c9b2d21ceb76c3c464e63387f3fd1
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-12-05 15:24:20 +0000

    additional config

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/config/ExtensionConfigurationProperties.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/config/ExtensionConfigurationProperties.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Configuration
 import java.time.LocalDate
 
 @ConfigurationProperties(prefix = "induction-extension")
@@ -14,7 +12,3 @@ data class InductionExtensionPeriod(
   var start: LocalDate = LocalDate.MIN,
   var end: LocalDate = LocalDate.MIN,
 )
-
-@Configuration
-@EnableConfigurationProperties(InductionExtensionConfig::class)
-class InductionExtensionConfigLoader
