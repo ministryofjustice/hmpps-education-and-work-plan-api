4f5bcfcf1a73ea9882e6edda261fe422743f054e
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-12-05 15:29:09 +0000

    additional config (#772)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/HmppsEducationAndWorkPlanApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/HmppsEducationAndWorkPlanApi.kt
@@ -2,14 +2,17 @@ package uk.gov.justice.digital.hmpps.educationandworkplanapi.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.config.InductionExtensionConfig
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
 @EnableAsync
+@EnableConfigurationProperties(InductionExtensionConfig::class)
 class HmppsEducationAndWorkPlanApi
 
 fun main(args: Array<String>) {
