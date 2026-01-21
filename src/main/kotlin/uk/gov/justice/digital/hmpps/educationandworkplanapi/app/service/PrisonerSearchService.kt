5abc5a085b5e9383e281ef38ff5bbae5856476d6
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2026-01-21 13:37:08 +0000

    small refactor prior to adding session searching (#801)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/PrisonerSearchService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/service/PrisonerSearchService.kt
@@ -1,10 +1,8 @@
 package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.service
 
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.client.prisonersearch.Prisoner
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.database.jpa.repository.PersonSearchRepository
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.PrisonerSearchController.PrisonerSearchCriteria
-import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.PaginationMetaData
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.PersonResponse
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.PersonSearchResult
 import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.PlanStatus
@@ -13,16 +11,9 @@ import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.Searc
 
 @Service
 class PrisonerSearchService(
-  private val prisonerSearchApiService: PrisonerSearchApiService,
+  prisonerSearchApiService: PrisonerSearchApiService,
   private val personSearchRepository: PersonSearchRepository,
-) {
-
-  private fun isPrisonNumber(prisonerNameOrNumber: String?): Boolean {
-    if (prisonerNameOrNumber.isNullOrBlank()) return false
-
-    val prnRegex = Regex("^[A-Z]\\d{4}[A-Z]{2}$")
-    return prnRegex.matches(prisonerNameOrNumber)
-  }
+) : AbstractPrisonerSearchService(prisonerSearchApiService) {
 
   fun searchPrisoners(
     searchCriteria: PrisonerSearchCriteria,
@@ -36,24 +27,6 @@ class PrisonerSearchService(
     val (metadata, pagedResponses) =
       sortedAndFilteredResponses.paginate(searchCriteria.page, searchCriteria.pageSize)
     return PersonSearchResult(metadata, pagedResponses)
-  }
-
-  fun <T> List<T>.paginate(
-    page: Int,
-    pageSize: Int,
-  ): Pair<PaginationMetaData, List<T>> {
-    val totalElements = size
-    val totalPages = (totalElements + pageSize - 1) / pageSize
-    val paged = drop((page - 1) * pageSize).take(pageSize)
-
-    return PaginationMetaData(
-      totalElements = totalElements,
-      totalPages = totalPages,
-      page = page,
-      pageSize = pageSize,
-      first = page == 1,
-      last = page == totalPages || paged.isEmpty(),
-    ) to paged
   }
 
   private fun List<PersonResponse>.filterByCriteria(searchCriteria: PrisonerSearchCriteria): List<PersonResponse> = this.filter { prisoner ->
@@ -117,24 +90,6 @@ class PrisonerSearchService(
       }
     }
     return personResponses
-  }
-
-  /**
-   * get the list of prisoners or if the prison number is supplied, only return that prisoner
-   */
-  private fun getPrisonerList(prisonerNameOrNumber: String?, prisonId: String): List<Prisoner> {
-    val prisonerList =
-      prisonerNameOrNumber
-        ?.takeIf { isPrisonNumber(it) }
-        ?.let { prisonNumber ->
-          prisonerSearchApiService
-            .getPrisoner(prisonNumber)
-            .takeIf { it.prisonId == prisonId }
-            ?.let { listOf(it) }
-            ?: emptyList()
-        }
-        ?: prisonerSearchApiService.getAllPrisonersInPrison(prisonId)
-    return prisonerList
   }
 
   private fun mapStatus(planStatus: String?): PlanStatus = when (planStatus) {
