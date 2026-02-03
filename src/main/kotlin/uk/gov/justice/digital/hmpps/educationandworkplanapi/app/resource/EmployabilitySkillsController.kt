de601a7115ae55e689eebd8130d13202cd6fa7ee
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-02-03 16:35:10 +0000

    Employability skills endpoint specs

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/EmployabilitySkillsController.kt
@@ -0,0 +1,48 @@
+package uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource
+
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Pattern
+import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.resource.validator.PRISON_NUMBER_FORMAT
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.CreateEmployabilitySkillsRequest
+import uk.gov.justice.digital.hmpps.educationandworkplanapi.resource.model.GetEmployabilitySkillResponses
+
+private val log = KotlinLogging.logger {}
+
+@RestController
+@Validated
+@RequestMapping(value = ["/action-plans/{prisonNumber}/employability-skills"], produces = [MediaType.APPLICATION_JSON_VALUE])
+class EmployabilitySkillsController {
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  @PreAuthorize(HAS_EDIT_ACTIONPLANS)
+  @Transactional
+  fun createEmployabilitySkills(
+    @Valid @RequestBody request: CreateEmployabilitySkillsRequest,
+    @PathVariable @Pattern(regexp = PRISON_NUMBER_FORMAT) prisonNumber: String,
+  ) {
+    TODO("Not yet implemented")
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  @PreAuthorize(HAS_VIEW_ACTIONPLANS)
+  fun getGoals(
+    @PathVariable @Pattern(regexp = PRISON_NUMBER_FORMAT) prisonNumber: String,
+  ): GetEmployabilitySkillResponses {
+    TODO("Not yet implemented")
+  }
+}
