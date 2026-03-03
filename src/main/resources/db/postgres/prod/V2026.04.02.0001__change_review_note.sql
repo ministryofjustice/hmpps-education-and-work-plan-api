713ede19d775abae85c73d172c60df37c865cd3b
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2026-03-03 13:23:26 +0000

    Remove notes for review added in error. (#850)

---
+++ src/main/resources/db/postgres/prod/V2026.04.02.0001__change_review_note.sql
@@ -0,0 +1,4 @@
+--Review added in error for this person change the timeline note as it was for another person RR-2369
+Update timeline_event_contextual_info set value = 'Review added in error' where timeline_event_id = 'f37a4e71-e6a9-4d35-a75e-f6b3fc5292d7' and name = 'COMPLETED_REVIEW_NOTES';
+--Review added in error for this person change the note as it was for another person RR-2369
+update note set content = 'Review added in error' where id = '9696f55f-7e74-4922-895d-e8fc8cba2297' and prison_number = 'A0856FK';
