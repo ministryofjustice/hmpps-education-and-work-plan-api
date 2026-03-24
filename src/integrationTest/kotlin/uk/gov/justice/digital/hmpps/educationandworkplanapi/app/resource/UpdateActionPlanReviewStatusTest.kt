e9fdb1950ba7202a05b275a348428bf2b074b9da
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2026-03-24 10:43:11 +0000

    RR-2388 - Update Review deadline when clearing exemption only if Review was not overdue when Review was exempted (#868)

--- src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/UpdateActionPlanReviewStatusTest.kt
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/educationandworkplanapi/app/resource/UpdateActionPlanReviewStatusTest.kt
@@ -34,6 +34,7 @@ import uk.gov.justice.digital.hmpps.educationandworkplanapi.app.database.jpa.ent
 class UpdateActionPlanReviewStatusTest : IntegrationTestBase() {
   companion object {
     private const val URI_TEMPLATE = "/action-plans/{prisonNumber}/reviews/schedule-status"
+    private val today = LocalDate.now()
   }
 
   private val prisonNumber = randomValidPrisonNumber()
@@ -269,8 +270,8 @@ class UpdateActionPlanReviewStatusTest : IntegrationTestBase() {
     createReviewScheduleRecord(
       prisonNumber,
       status = ReviewScheduleStatusEntity.SCHEDULED,
-      earliestDate = LocalDate.now().minusDays(5),
-      latestDate = LocalDate.now(),
+      earliestDate = today.minusDays(5),
+      latestDate = today,
     )
 
     // When
@@ -292,18 +293,20 @@ class UpdateActionPlanReviewStatusTest : IntegrationTestBase() {
     val reviewSchedule = reviewScheduleRepository.getAllByPrisonNumber(prisonNumber)
     assertThat(reviewSchedule.size).isEqualTo(1)
     assertThat(reviewSchedule[0].scheduleStatus.name).isEqualTo(ReviewScheduleStatus.SCHEDULED.name)
-    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(LocalDate.now().plusDays(5))
+    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(today.plusDays(5))
   }
 
   @Test
-  fun `when exception removed should add 5 days to latest review date `() {
+  fun `should add 5 days to latest review date when exemption removed given the review was not overdue when the exemption was applied`() {
     // Given
     createReviewScheduleRecord(
       prisonNumber,
       status = ReviewScheduleStatusEntity.EXEMPT_PRISONER_FAILED_TO_ENGAGE,
-      earliestDate = LocalDate.now().minusDays(5),
-      latestDate = LocalDate.now(),
+      earliestDate = today.minusDays(5),
+      latestDate = today, // review is not overdue
     )
+
+    val expectedDeadlineDate = today.plusDays(5)
 
     // When
     webTestClient.put()
@@ -324,18 +327,54 @@ class UpdateActionPlanReviewStatusTest : IntegrationTestBase() {
     val reviewSchedule = reviewScheduleRepository.getAllByPrisonNumber(prisonNumber)
     assertThat(reviewSchedule.size).isEqualTo(1)
     assertThat(reviewSchedule[0].scheduleStatus.name).isEqualTo(ReviewScheduleStatus.SCHEDULED.name)
-    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(LocalDate.now().plusDays(5))
+    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(expectedDeadlineDate)
   }
 
   @Test
-  fun `when exclusion removed should add 10 days to latest review date `() {
+  fun `should not add 5 days to latest review date when exemption removed given the review was already overdue when the exemption was applied`() {
+    // Given
+    createReviewScheduleRecord(
+      prisonNumber,
+      status = ReviewScheduleStatusEntity.EXEMPT_PRISONER_FAILED_TO_ENGAGE,
+      earliestDate = today.minusDays(6),
+      latestDate = today.minusDays(1), // review is already overdue
+    )
+
+    val expectedDeadlineDate = today.minusDays(1)
+
+    // When
+    webTestClient.put()
+      .uri(URI_TEMPLATE, prisonNumber)
+      .withBody(
+        aValidUpdateReviewScheduleStatusRequest(
+          prisonId = "MDI",
+          status = ReviewScheduleStatus.SCHEDULED,
+        ),
+      )
+      .bearerToken(aValidTokenWithAuthority(REVIEWS_RW, username = "auser_gen", privateKey = keyPair.private))
+      .contentType(APPLICATION_JSON)
+      .exchange()
+      .expectStatus()
+      .isNoContent
+
+    // Then
+    val reviewSchedule = reviewScheduleRepository.getAllByPrisonNumber(prisonNumber)
+    assertThat(reviewSchedule.size).isEqualTo(1)
+    assertThat(reviewSchedule[0].scheduleStatus.name).isEqualTo(ReviewScheduleStatus.SCHEDULED.name)
+    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(expectedDeadlineDate)
+  }
+
+  @Test
+  fun `should add 10 days to latest review date when exclusion removed given the review was not overdue when the exclusion was applied`() {
     // Given
     createReviewScheduleRecord(
       prisonNumber,
       status = ReviewScheduleStatusEntity.EXEMPT_PRISON_REGIME_CIRCUMSTANCES,
-      earliestDate = LocalDate.now().minusDays(5),
-      latestDate = LocalDate.now(),
+      earliestDate = today.minusDays(5),
+      latestDate = today, // review is not overdue
     )
+
+    val expectedDeadlineDate = today.plusDays(10)
 
     // When
     webTestClient.put()
@@ -356,13 +395,47 @@ class UpdateActionPlanReviewStatusTest : IntegrationTestBase() {
     val reviewSchedule = reviewScheduleRepository.getAllByPrisonNumber(prisonNumber)
     assertThat(reviewSchedule.size).isEqualTo(1)
     assertThat(reviewSchedule[0].scheduleStatus.name).isEqualTo(ReviewScheduleStatus.SCHEDULED.name)
-    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(LocalDate.now().plusDays(10))
+    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(expectedDeadlineDate)
+  }
+
+  @Test
+  fun `should not add 10 days to latest review date when exclusion removed given the review was already overdue when the exclusion was applied`() {
+    // Given
+    createReviewScheduleRecord(
+      prisonNumber,
+      status = ReviewScheduleStatusEntity.EXEMPT_PRISON_REGIME_CIRCUMSTANCES,
+      earliestDate = today.minusDays(6),
+      latestDate = today.minusDays(1), // review is already overdue
+    )
+
+    val expectedDeadlineDate = today.minusDays(1)
+
+    // When
+    webTestClient.put()
+      .uri(URI_TEMPLATE, prisonNumber)
+      .withBody(
+        aValidUpdateReviewScheduleStatusRequest(
+          prisonId = "MDI",
+          status = ReviewScheduleStatus.SCHEDULED,
+        ),
+      )
+      .bearerToken(aValidTokenWithAuthority(REVIEWS_RW, username = "auser_gen", privateKey = keyPair.private))
+      .contentType(APPLICATION_JSON)
+      .exchange()
+      .expectStatus()
+      .isNoContent
+
+    // Then
+    val reviewSchedule = reviewScheduleRepository.getAllByPrisonNumber(prisonNumber)
+    assertThat(reviewSchedule.size).isEqualTo(1)
+    assertThat(reviewSchedule[0].scheduleStatus.name).isEqualTo(ReviewScheduleStatus.SCHEDULED.name)
+    assertThat(reviewSchedule[0].latestReviewDate).isEqualTo(expectedDeadlineDate)
   }
 
   @Test
   fun `Test follow on events occur`() {
     // Given
-    val today = LocalDate.now()
+    val today = today
     val fiveDaysAgo = today.minusDays(5)
     wiremockService.stubGetPrisonerFromPrisonerSearchApi(prisonNumber, aValidPrisoner(prisonNumber))
     // Create a review schedule record, scheduled to be completed by today
